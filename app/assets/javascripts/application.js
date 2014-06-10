a3d9339fb489b5d8cb796c4d1bba982bc34b9c9c
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-06-10 02:03:55 -0700

    adding results of generators, plus some support stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//
+// Required by Blacklight
+//= require blacklight/blacklight
 //= require_tree .
+//= require sufia
+
---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'dae5e69425cb3159e6c6b650f3810984291ba5e0e31aa8a3090238f2c0580bc17835c9213f922769d591dec7e85d189d4e2256cd080b9cc4cd7ebe91846cfd30'
+
